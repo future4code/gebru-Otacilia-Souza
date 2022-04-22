c59807fe0cf7ecc746ba5868836363aeefae0aa2
Otacilia <otaciliatimoteo@hotmail.com>
2022-04-21 21:53:03 -0300

    continuação dos exercicos

--- revisãojs-postinho/revisãoJs-postinho/index.js
+++ revisãojs-postinho/revisãoJs-postinho/index.js
@@ -27,22 +27,40 @@ console.log(checarDesigualdade(1, 2));
 // b-)Compare a igualdade entre a===b
 
 function checarIgualdade(a, b) {
-    //  Sua lógica aqui
-}
+    
+        return  `NO comparador de igualdade ${a} ===${b} é ${a === b}`
+        }
+        console.log(checarIgualdade(358,65));
+
 
 // c-)Faça uma função chamada "verificaSeEMaior"
 
-console.log(verificaSeEMaior(321, 2156));
+function verificarSeMaior(a, b){
+
+
+
+    if(a >b){
+        return " numero a, true"
+    }
+  
+  else {
+    return " b,  true"
+  }
+  }
+  
+  console.log(verificarSeMaior(321, 2156));
+  
+  
 
 
 // Exercício 2------------------------------------------------------------------------------------
 //Escreva true or false para as comparações abaixo:
 // exemplo 1>2 = false
-// a-) 1==='1'=
-// b-) 1=='1'=
-// c-) 'a'==='b'=
-// d-) 'b'>'a'=
-// e-) 0!==null=
+// a-) 1==='1'= false
+// b-) 1=='1'= true
+// c-) 'a'==='b'=true
+// d-) 'b'>'a'= false
+// e-) 0!==null= true
 
 
 // CONDICIONAIS
@@ -50,23 +68,63 @@ console.log(verificaSeEMaior(321, 2156));
 // Exercício 3------------------------------------------------------------------------------------
 
 const cadastro = () => {
-    const usuario = []
+    var usuario = {
+        'nome': null,
+        'anoNascimento': null,
+        'senha': null,
+        'nacionalidade': null,
+    };
+    
+    const anoAtual = (new Date()).getFullYear();
+    
+    
+    usuario.nome = prompt('Digite o nome do usuário:');
+    usuario.anoNascimento = prompt('Digite o ano de nascimento do usuário:');
+    
+    if((parseInt(usuario.anoNascimento) + 18) <= anoAtual){
+    
+        usuario.senha = prompt('Digite a senha do usuário com no maximo 6 caracteres:');
+        while(usuario.senha.length > 6){
+            usuario.senha = prompt('Digite a senha do usuário com no maximo 6 caracteres:');
+        }
+    
+        if(confirm("Essa é sua senha: '"+usuario.senha+"' ?")){
+            usuario.nacionalidade = prompt('Digite a nascionalidade do usuário:');
+    
+            if(usuario.nacionalidade.toUpperCase() === 'BRASILEIRA' || usuario.nacionalidade.toUpperCase() === 'BRASILEIRO'){
+                alert('Cadastro concluído com sucesso!');
+            }else{
+                alert('Cadastro permitido apenas para brasileiros');
+            }
+        }else{
+            alert('Reinicie o cadastro!');
+        }
+    }else{
+        alert('O cadastro só é permitido para maiores de 18 anos.');
+    }
 
-    //  Sua lógica aqui
 
-}
+    return Object.keys(usuario).map((key) => usuario[key]);
+
 console.log(cadastro());
 
 // Exercício 4-----------------------------------------------------------------------------------------------
-
 const login = () => {
-    const login = "labenu"
-    //  Sua lógica aqui
 
-}
-
-console.log(login());
-
+    const login="labenu"
+    const senhaUsuario = prompt("Digite sua senha")
+    if(senhaUsuario === login){
+      console.log("usuario logado")
+    } else{ console.log("senha invalida")
+  };
+    
+    return "login"
+    
+    }
+    
+    console.log(login());
+  
+    
 // Exercício 5----------------------------------------------------------------------------------------------------
 
 const primeiraDose = () => {
@@ -82,18 +140,17 @@ console.log(primeiraDose())
 
 // Exercício 6 -------------------------------------------------------------------------------------
 
-const segundaDose = (nomeDoUsuario) => {
-    const usuarios = [
-        { nome: "Artur", imunizacao: "incompleta" },
+const segundaDose = (nomeDoUsuario) => [
+       { nome: "Artur", imunizacao: "incompleta" },
         { nome: "Barbara", imunizacao: "incompleta" },
         { nome: "Carlos", imunizacao: "incompleta" }
     ]
 
-    //  Sua lógica aqui
 
 
 }
-console.log(segundaDose("Barbara"));
+console.log(segundaDose("Barbara"));  
+   
 
 // Exercício 7 --------------------------------------------------------------------------------------
 
@@ -103,12 +160,15 @@ const avisoAosAtrasados = () => {
         { nome: "Barbara", imunizacao: "completa" },
         { nome: "Carlos", imunizacao: "incompleta" }
     ]
-
-    //  Sua lógica aqui
-
-}
-console.log(avisoAosAtrasados());
-
+    for(let i = 0; i< usuarios.length; i++){
+      if(usuarios[i].imunizacao =="incompleta"){
+        console.log (`ola ${usuarios[i].nome} Suua imunização esta ${usuarios[i].imunizacao}
+        por favor volte ao posto para tomar a segunda dose`)
+      }
+    }
+  }
+  console.log(avisoAosAtrasados());
+  
 
 // DESAFIO------------------------------------------------------------------------------------------
 
