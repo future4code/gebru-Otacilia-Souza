3b6f9649d2c03549af68a761fc752552e7604f96
Otacilia <otaciliatimoteo@hotmail.com>
2022-04-26 23:25:33 -0300

    mudança de tela

---
+++ star-wars-app2/src/App.js
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+import CharacterDetailPage from "./CharacterDetailPage/CharacterDetailPage";
+import CharacterListPage from "./CharacterListPage/CharacterListPage";
+
+
+
+
+function AppStarWars() {
+  const [ currentPage, setCurrentPage] = useState("list")
+
+function selectePage () {
+if (currentPage === "details") {
+  return  <CharacterDetailPage/> } 
+else  { return   <CharacterListPage/>}
+
+
+}
+
+return (
+  <div>
+    <h1>Star Wars</h1>
+   {selectePage ()}
+ </div>
+)
+}
+
+export default AppStarWars;
