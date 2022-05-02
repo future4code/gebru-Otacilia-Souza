dbfb29fb85cd74cba2df10b7818e1f929237d79c
Otacilia <otaciliatimoteo@hotmail.com>
2022-05-01 23:08:46 -0300

    corrigido o redirecionamento de paginas, feito o styled e  acrescentado o index.html

--- star-wars-app-reforco/src/CharacterListPage/CharacterListPage.js
+++ star-wars-app-reforco/src/CharacterListPage/CharacterListPage.js
@@ -1,12 +1,36 @@
-
-
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios"
+import { BASE_URL } from "../constants/url";
+import { CharacterCard } from "./styledListaPage";
 
 const CharacterListPage = () =>{
+ const [CharacterList, setCharacterList] = useState([])
+
+    function getCharacterList(){
+    axios.get(`${BASE_URL}/people/`)
+    .then((response) => setCharacterList (response.data.results))
+    .catch((error) => console.log("erro", error.message))
+}
+
+useEffect(() => {
+    getCharacterList()
+     }, []);
+
+const showCharacters = () => {
+    return CharacterList.map((Character) => {
+        return <CharacterCard key={Character.url}>{Character.name}</CharacterCard>
+    });
+
+}
+
+getCharacterList()
+
+
     return (
 <div>
    <h5>teste componente ListaPage</h5>
+   {showCharacters()}
 </div>
-    )
+    );
 } 
 export default CharacterListPage
---
+++ star-wars-app-reforco/src/CharacterListPage/styledListaPage.js
@@ -0,0 +1,19 @@
+
+
+
+
+import styled from "styled-components"
+
+export const CharacterCard = styled.div`
+    border-radius: 6px;
+    background-color: #996633;
+
+padding: 10px;
+margin: 10px;
+width:  300px;
+&:hover{
+    cursor:pointer;
+    background: linear-gradient(#663300, #996633, #999966);
+}
+
+`
