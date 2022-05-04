d3b870698cd83ff360a4ed962b8f1939c8f1fc8e
Otacilia <otaciliatimoteo@hotmail.com>
2022-05-03 22:07:05 -0300

    detalhes da pagina

--- star-wars-app-reforco/src/CharacterDetailsPage/CharacterDetailsPage.js
+++ star-wars-app-reforco/src/CharacterDetailsPage/CharacterDetailsPage.js
@@ -1,47 +1,41 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const CharacterDetailsPage = (props) =>{
-const [details, setDetails] = useState ({})
-const [planet, setPlanet] = useState({})
-
-// useEffect(() => {
-//     getCharacterDateils()
-// }, [])
-
-// useEffect(() => {
-// getPlanetDatails()
-// },[details.homeworld])
-
-const getCharacterDateils=()=>{
-    axios.get(props.url)
-    .then((response)=>{setDetails(response.data.results)})
-    .catch((error) => console.log("erro", error.message))}
-
-    function getPlanetDatails (){
-    axios.get(details.homeworld)
-    .then((response)=>{setPlanet(response.data.results)})
-    .catch((error) => console.log("erro", error.message))}
+const CharacterDetailsPage = (props) => {
+    const [details, setDetails] = useState([])
+    const [planet, setPlanet] = useState([])
 
     useEffect(() => {
         getCharacterDateils()
     }, [])
 
-useEffect(() => {
-    getPlanetDatails()
-    },[])
+    useEffect(() => {
+        getPlanetDatails()
+    }, [])
 
-    console.log(details)
-    console.log(planet)
+
+    const getCharacterDateils = () => {
+        
+        axios.get(props.url)
+            .then((response) => { setDetails(response.data) })
+            .catch((error) => console.log("erro", error.message))
+    }
+
+    const getPlanetDatails = () => {
+        axios.get(details.homeworld)
+            .then((response) => { setPlanet(response.data) })
+            .catch((error) => console.log("erro", error.message))
+    }
+console.log(planet)
 
 
     return (
-<div>
-    <h1>Detalhes dos personagens</h1>
-   <p>Nome: {details.name}</p> 
-   <p>Planeta de Origem: {planet.name}</p>
-   <button onClick={props.goToListpage}>voltar para a pagina de personagens</button>
-</div>
+        <div>
+            <h1>Detalhes dos personagens</h1>
+             <p>Nome: {details.name}</p>
+            <p>Planeta de Origem: {planet.name}</p>
+            <button onClick={props.goToListpage}>voltar para a pagina de personagens</button> 
+        </div>
     )
 }
 
