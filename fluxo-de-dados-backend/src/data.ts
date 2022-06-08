11648d4f09322bc7cb05a3e7bc89f257f0b01840
OtaciliaSouza <otaciliatimoteo@hotmail.com>
2022-06-08 15:46:24 -0300

    fluxo de dados

---
+++ fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,10 @@
+type data = {id:string , name:string , price :number }
+
+
+export const data: data[] = [ 
+    {id:"1", name: "espresso", price : 5.00},
+    {id:"2", name: "mocha", price : 10.00},
+    {id:"3", name: "macchiato", price : 12.00},
+    {id:"4", name: "cappuccino", price : 8.00}
+
+]
