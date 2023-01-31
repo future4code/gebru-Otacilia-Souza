730958b06832757d2bf2afda1eabc1c2615849fb
OtaciliaSouza <otaciliatimoteo@hotmail.com>
2023-01-31 12:51:55 -0300

    case promobit

---
+++ case2/personare/src/components/Container/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+
+export const Container = styled.main`
+ width:100%;
+ display:grid;
+ grid-template-columns: repeat(auto-fit, minmax( 186px, 1fr));
+ gap: 30px;padding: 2%;
+ background-color: #EEDC82;
+`
